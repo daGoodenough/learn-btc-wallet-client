a9cab04e1293238de6261bb5ab77ab708ad9d8a8
Damian Goodenough <dgoodenough@proton.me>
2023-02-02 13:26:38 -0800

    Set up an offcanvas in the header

--- src/app/TopNav.js
+++ src/app/TopNav.js
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Offcanvas } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -13,9 +13,42 @@ const TopNav = () => {
   }
 
   return (
-    <Navbar variant="dark" expand="lg" collapseOnSelect>
+    <Navbar variant="dark" expand="xs" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="#home">Learn BTC Wallet</Navbar.Brand>
+        <div>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+          <Navbar.Brand href="#home">Learn BTC Wallet</Navbar.Brand>
+        </div>
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-sm`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown
+                title="Dropdown"
+                id={`offcanvasNavbarDropdown-expand-sm`}
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
         {
           username ? (
             <NavDropdown title={username} id="basic-nav-dropdown">
