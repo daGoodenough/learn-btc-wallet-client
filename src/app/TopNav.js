e15db421b2ac7f4c7023800196de4a03da84b4e1
Damian Goodenough <dgoodenough@proton.me>
2023-02-02 12:55:32 -0800

    conditionally render username of "Login" for header

--- src/app/TopNav.js
+++ src/app/TopNav.js
@@ -1,30 +1,40 @@
-import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { signout } from '../features/auth/authSlice';
 
 const TopNav = () => {
+  const dispatch = useDispatch();
+  const { username } = useSelector((state) => state.auth);
+
+  const handleSignout = () => {
+    dispatch(signout())
+  }
+
   return (
     <Navbar variant="dark" expand="lg" collapseOnSelect>
       <Container>
         <Navbar.Brand href="#home">Learn BTC Wallet</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className='' id="basic-navbar-nav">
-          <Nav className="me-auto navbar-right">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">+Key</Nav.Link>
-            <Nav.Link href="#link">+Wallet</Nav.Link>
-            <Nav.Link href="#link">+Transaction</Nav.Link>
-            <NavDropdown title="Username" id="basic-nav-dropdown">
+        {
+          username ? (
+            <NavDropdown title={username} id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Keys</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 TX History
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Wallets</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item onClick={handleSignout}>
                 Signout
               </NavDropdown.Item>
             </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+          ) : (
+            <Nav>
+              <Nav.Link className="nav-login" as={Link} to={'/login'}>Login</Nav.Link>
+            </Nav>
+          )
+        }
       </Container>
     </Navbar>
   );
