31a935c9dd977aa9a50e156b40ae68e71ca690c2
Damian Goodenough <dgoodenough@proton.me>
2023-02-03 15:23:54 -0800

    create function to generate pub key, compressed or not

--- src/features/keys/keyUtils.js
+++ src/features/keys/keyUtils.js
@@ -3,7 +3,22 @@ const BASE_URL = process.env.REACT_APP_API_HOST
 
 export const generatePrivKey = async () => {
   const response = await
-  axios
-    .get(`${BASE_URL}/api/keys/private`)
+    axios
+      .get(`${BASE_URL}/api/keys/private`)
+  return response.data;
+};
+
+export const generatePubKey = async (privateKey, compressed) => {
+  const response = await
+    axios.post(`${BASE_URL}/api/keys/public`, {
+      privateKey
+    }, {
+      params: {
+        compressed
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   return response.data;
 }
