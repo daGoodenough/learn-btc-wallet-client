f2474afaf25e578b2df6be715bcaeab96d8fc32c
Damian Goodenough <dgoodenough@proton.me>
2023-02-03 14:25:56 -0800

    add some forms to hold the private and pub keys

--- src/features/keys/CreateKeyModal.js
+++ src/features/keys/CreateKeyModal.js
@@ -1,6 +1,18 @@
-import {Modal, Button} from 'react-bootstrap';
+import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+
+import {generatePrivKey} from './keyUtils';
 
 const CreateKeyModal = (props) => {
+  const [privKey, setPrivKey] = useState('');
+  const [pubKey, setPubKey] = useState('');
+
+  const handleGeneratePrivKeyClick = async(e) => {
+    e.preventDefault();
+    const privKeyObject = await generatePrivKey();
+    setPrivKey(privKeyObject.privateKey);
+  }
+
   return (
     <Modal
       {...props}
@@ -15,22 +27,49 @@ const CreateKeyModal = (props) => {
       </Modal.Header>
       <Modal.Body>
         <h4>Step 1: Generate a Private Key</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-        <h4>Step 2: Generate a Public Key</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <Form>
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Private Key</Form.Label>
+              <Form.Control
+                as="textarea"
+                value={privKey}
+                disabled
+              />
+            </Form.Group>
+            <Col 
+              className="d-flex justify-content-md-center align-items-center" 
+              md={2} sm={12}
+            >
+              <button onClick={(e => handleGeneratePrivKeyClick(e))}>Generate</button>
+            </Col>
+          </Row>
+        </Form>
+        <h4>Step 2: Generate Corresponding Public Key</h4>
+        <Form>
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Private Key</Form.Label>
+              <Form.Control
+                value={pubKey}
+                onChange={(e) => setPubKey(e.target.value)}
+                type="text"
+                placeholder=""
+              />
+            </Form.Group>
+            <Col
+              className="d-flex justify-content-center align-items-center"
+              md={2} sm={12}
+            >
+              <button>Generate</button>
+            </Col>
+          </Row>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
-    </Modal>
+    </Modal >
   );
 }
 
