127b807c3de362e8c6bd9dc5b700788c5772dc52
Damian Goodenough <dgoodenough@proton.me>
2023-02-06 18:48:00 -0800

    add key

--- src/features/offcanvas/OffCanvasListItem.js
+++ src/features/offcanvas/OffCanvasListItem.js
@@ -13,17 +13,19 @@ const OffCanvasListItem = ({ item, itemType, placeholder }) => {
         <button className="offcanvas-list-item" onClick={() => setModalShow(true)}>+ Create a {itemType}</button>
         {itemType === 'key' &&
           <CreateKeyModal
+            key={itemType}
             show={modalShow}
             onHide={() => setModalShow(false)}
           />
-          }
-          {
-            itemType === 'wallet' &&
-            <CreateWalletModal
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-            />
-          }
+        }
+        {
+          itemType === 'wallet' &&
+          <CreateWalletModal
+            key={itemType}
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        }
       </>
     )
   };
