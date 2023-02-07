a3a323ff6eafc52a25a9d7d814a0dc611dfce6b8
Damian Goodenough <dgoodenough@proton.me>
2023-02-07 12:09:37 -0800

    create keys list for home page

---
+++ src/features/homepage/KeysList.js
@@ -0,0 +1,37 @@
+import { Table } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
+import KeyInfoModal from '../keys/KeyInfoModal';
+
+const KeysList = () => {
+  const { keys } = useSelector(state => state);
+  const [modalShow, setModalShow] = useState(false);
+
+  if (!keys) {
+    return <div>No Keys</div>
+  }
+
+  return (
+    <Table>
+      <tbody>
+        {keys.map(key => {
+          return (
+            <tr onClick={() => setModalShow(!modalShow)}>
+              <td>{key.keyName}</td>
+              <td>{key.publicKey}</td>
+              <KeyInfoModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                keyPair={key}
+                key={key._id}
+              />
+            </tr>
+          )
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default KeysList;
