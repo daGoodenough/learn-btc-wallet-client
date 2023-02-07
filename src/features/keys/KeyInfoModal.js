52436b93985e41a0878f75fa9f8c8caa3da98cd5
Damian Goodenough <dgoodenough@proton.me>
2023-02-07 10:09:08 -0800

    render KeyInfoModal when key row is clicked

---
+++ src/features/keys/KeyInfoModal.js
@@ -0,0 +1,66 @@
+import { Modal, Row, Form, Col } from 'react-bootstrap';
+
+const KeyInfoModal = (props) => {
+  const {keyPair} = props;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {keyPair?.keyName}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Group className="mb-3">
+          <Form.Label>Private Key</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={keyPair?.privateKey}
+            disabled
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Public Key</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={keyPair?.publicKey}
+            disabled
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>WIF</Form.Label>
+          <Form.Control
+            as="textarea"
+            value={keyPair?.wif}
+            disabled
+          />
+        </Form.Group>
+        <Row className='justify-content-between text-center'>
+          <Form.Group as={Col} md={3} sm={12}className="mb-3">
+            <Form.Label>Compressed?</Form.Label>
+            <Form.Control
+              value={keyPair?.compressed}
+              disabled
+            />
+          </Form.Group>
+          <Form.Group as={Col} md={3} sm={12} className="mb-3">
+            <Form.Label>Network</Form.Label>
+            <Form.Control
+              value={keyPair?.network}
+              disabled
+            />
+          </Form.Group>
+        </Row>
+      </Modal.Body>
+      <Modal.Footer className='justify-content-around'>
+        Click on any field to learn more.
+      </Modal.Footer>
+    </Modal >
+  );
+}
+
+export default KeyInfoModal;
--- src/features/wallets/WalletKeyList.js
+++ src/features/wallets/WalletKeyList.js
@@ -1,38 +1,52 @@
 import { Table, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { useState } from 'react'
+import KeyInfoModal from '../keys/KeyInfoModal';
 
 const WalletKeyList = ({ keyIds }) => {
   const userKeys = useSelector(state => state.keys);
   const addrKeys = keyIds?.map(keyId => userKeys.find(key => key._id === keyId));
+  const [modalShow, setModalShow] = useState(false);
 
   if (!addrKeys || addrKeys.length === 0) {
-    return (<></>)
+    return (<>No Keys</>)
   };
   return (
-    <Col>
-    <Table variant='dark' borderless>
-      <tbody>
-        {addrKeys.map(key => {
-          console.log(key)
-          return (
-            <tr className='d-flex justify-content-between' key={key._id}>
-              <td className='key-name'>
-                <div>
-                  {key.keyName}
-                </div>
-              </td>
-              <td>
-                <div className='pub-key'>
-                  {key.publicKey}
-                </div>
-                <div className='sub-descriptor'>Pub Key</div>
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-    </Col>
+    <>
+      <Col>
+        <Table variant='dark' borderless hover>
+          <tbody>
+            {addrKeys.map(key => {
+              return (
+                <tr
+                  className='d-flex justify-content-between'
+                  key={key._id}
+                  onClick={() => setModalShow(!modalShow)}
+                >
+                  <td className='key-name'>
+                    <div>
+                      {key.keyName}
+                    </div>
+                  </td>
+                  <td>
+                    <div className='pub-key'>
+                      {key.publicKey}
+                    </div>
+                    <div className='sub-descriptor'>Pub Key</div>
+                  </td>
+                  <KeyInfoModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                    keyPair={key}
+                    key={key._id}
+                  />
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </Col>
+    </>
   );
 }
 
